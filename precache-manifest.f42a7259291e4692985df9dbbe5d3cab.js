6cd3359e1ec18bb92c481598a3b11032fd959d7f
Fatima Altaf <fatima.altaf@gmail.com>
2020-04-22 15:58:36 -0400

    Updates

---
+++ precache-manifest.f42a7259291e4692985df9dbbe5d3cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde670cc13ddf719e12738e9ac634257",
+    "url": "/2020-04-22-Noisemaker/index.html"
+  },
+  {
+    "revision": "e5db1f23def0013308d5",
+    "url": "/2020-04-22-Noisemaker/static/css/main.f18f3fd9.chunk.css"
+  },
+  {
+    "revision": "6b687097e1e0192532a5",
+    "url": "/2020-04-22-Noisemaker/static/js/2.dc509577.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/2020-04-22-Noisemaker/static/js/2.dc509577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5db1f23def0013308d5",
+    "url": "/2020-04-22-Noisemaker/static/js/main.e8211b28.chunk.js"
+  },
+  {
+    "revision": "e9c263f69e667b40def4",
+    "url": "/2020-04-22-Noisemaker/static/js/runtime-main.c7ae0f90.js"
+  }
+]);
