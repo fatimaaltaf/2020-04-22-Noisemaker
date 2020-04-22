80b434597dcea04120c450a698af2363c6859408
Fatima Altaf <fatima.altaf@gmail.com>
2020-04-22 13:13:58 -0400

    initial commit

---
+++ noisemaker/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2>Pianope</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
